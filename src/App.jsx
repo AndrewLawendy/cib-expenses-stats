aa95fea3312322e201daf42c0315eabc3fc028c4
andrew-hani <andrew.lawendy@outlook.com>
2020-11-09 13:50:49 +0200

    🎉 Init Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <h1>Working...</h1>
+    </React.Fragment>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
