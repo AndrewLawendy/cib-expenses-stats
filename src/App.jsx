669302f2ba72ffabefd9c170adbd48e4edbeffc6
andrew-hani <andrew.lawendy@outlook.com>
2020-11-09 23:10:42 +0200

    🎨 Adding Css modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 
 import "semantic-ui-css/semantic.min.css";
 
+import Nav from "./components/nav/Nav.jsx";
+
 const App = () => {
   return (
     <React.Fragment>
-      <h1>Working...</h1>
+      <Nav />
     </React.Fragment>
   );
 };
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./styles.scss";
+
+const Nav = () => {
+  return <div className={styles.nav}>Nav</div>;
+};
+
+export default Nav;
