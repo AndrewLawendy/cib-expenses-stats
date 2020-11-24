14a5a8174464e521b1457913b338d30392e2abb6
andrew-hani <andrew.lawendy@outlook.com>
2020-11-24 14:10:23 +0200

    ✨ Support zero payment days

--- src/components/expensesLineChart/ExpensesLineChart.jsx
+++ src/components/expensesLineChart/ExpensesLineChart.jsx
@@ -9,6 +9,10 @@ import { AppContext } from "../appContext/AppContext.jsx";
 
 const ExpensesLineChart = () => {
   const { jsonData } = useContext(AppContext);
+  const [firstRecord] = jsonData;
+  const [, month, year] = firstRecord.date.split("/");
+  const lastDay = new Date(year, month, 0).getDate();
+
   const summedData = jsonData.reduce((acc, { date, description, amount }) => {
     if (!acc[date]) acc[date] = { amount: 0, description: "" };
     acc[date].amount += amount;
@@ -16,9 +20,15 @@ const ExpensesLineChart = () => {
 
     return acc;
   }, {});
-  const chartData = Object.keys(summedData).map((summedDate) => {
-    const { amount, description } = summedData[summedDate];
-    const [day, month, year] = summedDate.split("/");
+
+  const chartData = Array.from({ length: lastDay }, (_, index) => {
+    const day = (index + 1).toString().padStart(2, "0");
+    const stringDate = `${day}/${month}/${year}`;
+    const summedDataValue = summedData[stringDate] || {
+      amount: 0,
+      description: "No payment that day\n",
+    };
+    const { amount, description } = summedDataValue;
     const date = new Date(`${month}/${day}/${year}`);
 
     return { date, amount, description };
