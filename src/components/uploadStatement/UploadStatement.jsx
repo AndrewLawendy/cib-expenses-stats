52816a6f2441ff765469351584b7862bdd16fa7e
andrew-hani <andrew.lawendy@outlook.com>
2020-11-29 22:39:01 +0200

    🎨 Reading xls file and basic convert into json

--- src/components/uploadStatement/UploadStatement.jsx
+++ src/components/uploadStatement/UploadStatement.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import excelToJson from "convert-excel-to-json";
+import { encode } from "base64-arraybuffer";
 
 import styles from "./styles.scss";
 
 const UploadStatement = () => {
   const { getRootProps, getInputProps } = useDropzone({
-    accept: ".pdf",
+    accept: ".xls",
     multiple: false,
     onDrop: handleFileDropped,
   });
 
-  function handleFileDropped([statementFile]) {}
+  function handleFileDropped([statementFile]) {
+    const reader = new FileReader();
+    reader.onload = ({ target: { result } }) => {
+      const { Sheet1 } = excelToJson({
+        source: encode(result),
+      });
+      console.log(Sheet1);
+    };
+    reader.readAsArrayBuffer(statementFile);
+  }
 
   return (
     <div {...getRootProps({ className: styles.dropZone })}>
